a9050a7d54e09a48a5dd96bd7aa2cba80faa6193
MondeAnna <monde.anna@gmail.com>
2024-03-09 13:25:30 +0200

    proj (wip): integrate base model and storage
        - `models/__init__.py` structured as to prevent
          circular imports when running tests
    
        - solution to migrate all imports to top of module
          yet to be found

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
-from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
 storage = FileStorage()
 storage.reload()
+
+
+from models.base_model import BaseModel
--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,9 @@ from datetime import datetime
 import uuid
 
 
+from models import storage
+
+
 class BaseModel:
 
     """
@@ -40,6 +43,7 @@ class BaseModel:
         """Update the `updated_at` attribute"""
 
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Serialises BaseModel's attributes into a `dict`"""
@@ -60,6 +64,7 @@ class BaseModel:
 
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
+        storage.new(self)
 
     def __init_kwargs(self, kwargs):
         """
