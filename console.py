90dd9b43af31872700f7b3db2df40028dcfbaf16
MondeAnna <monde.anna@gmail.com>
2024-03-09 14:27:34 +0200

    console (method): add creation of instance
        - upon creation
            - instance is saved
            - id is printed to console

--- console.py
+++ console.py
@@ -7,6 +7,9 @@ Command Line Interface for the product
 import cmd
 
 
+from models import BaseModel
+
+
 class HBNBCommand(cmd.Cmd):
 
     """
@@ -30,6 +33,44 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program"""
 
         return True
+
+    def do_create(self, class_name):
+        """
+        Creates a new instance, saves it and prints the id. If
+        the class name is missing, informs the user and if the
+        class name does not exist, informs the user.
+
+        Parameter
+        ---------
+        class_name : str
+            name of class to create
+
+        Expected
+        --------
+            (hbnb) create BaseModel
+
+        Missing class name
+        ------------------
+            (hbnb) create
+            ** class name missing **
+
+        Non-Existant Class
+        ------------------
+            (hbnb) create DoesNotExist
+            ** class does'nt exist **
+        """
+
+        available_models = ["BaseModel"]
+
+        if not class_name:
+            return print("** class name missing **")
+
+        if class_name not in available_models:
+            return print("** class doesn't exist **")
+
+        model = BaseModel()
+        model.save()
+        print(model.id)
 
 
 if __name__ == "__main__":
