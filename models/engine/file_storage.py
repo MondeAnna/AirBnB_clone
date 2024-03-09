0ecc49dd09de0ca76787a654e521afecb89163d8
MondeAnna <monde.anna@gmail.com>
2024-03-09 08:25:03 +0200

    file (method): `new` updates objects being tracked for serialisation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,3 +25,34 @@ class FileStorage:
         """Provides all the saved BaseModel objects"""
 
         return self.__objects
+
+    def new(self, obj):
+        """
+        Updates tracked items with the retrieval key of
+        <obj class name>.id
+
+        Parameter
+        ---------
+        obj : BaseModel | subclass(BaseModel)
+            object to be tracked
+        """
+
+        key = self.__extract_key(obj)
+        self.__objects[key] = obj
+
+    @staticmethod
+    def __extract_key(obj):
+        """
+        Updates tracked items with the retrieval key of
+        <obj class name>.id
+
+        Parameter
+        ---------
+        obj : Any
+            object to be tracked
+        """
+
+        dict_ = obj.to_dict()
+        cls = dict_.get("__class__")
+        id_ = dict_.get("id")
+        return f"{cls}.{id_}"
--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -6,6 +6,7 @@ Test suite for saving BaseModel to file system
 """
 
 
+from unittest.mock import MagicMock
 from unittest.mock import patch
 from unittest import TestCase
 from unittest import main
@@ -22,7 +23,21 @@ class TestFileStorage(TestCase):
     """
 
     def setUp(self):
-        self.storage_empty = FileStorage()
+        """Preparatory object structure needed to carry out tests"""
+
+        self.storage = FileStorage()
+        self.mock_model_00 = MagicMock(id="mock_model_00")
+        self.mock_model_01 = MagicMock(id="mock_model_01")
+
+        self.mock_model_00.to_dict.return_value = {
+            "__class__": "BaseModel",
+            "id": "mock_model_00",
+        }
+
+        self.mock_model_01.to_dict.return_value = {
+            "__class__": "BaseModel",
+            "id": "mock_model_01",
+        }
 
 
 class TestAll(TestFileStorage):
@@ -32,7 +47,23 @@ class TestAll(TestFileStorage):
     def test_all_with_empty_storage(self):
         """Assert that a new instance has no saved objects"""
 
-        self.assertEqual(self.storage_empty.all(), {})
+        self.assertEqual(self.storage.all(), {})
+
+
+class TestNew(TestFileStorage):
+
+    """Assert update of objects being tracked for serialisation"""
+
+    def test_new_when_single_object_is_provided(self):
+        """Assert that single provided instance is tracked"""
+
+        self.storage.new(self.mock_model_00)
+        objects = self.storage.all()
+
+        print(objects)
+
+        self.assertTrue("BaseModel.mock_model_00" in objects.keys())
+        self.assertTrue(self.mock_model_00 in objects.values())
 
 
 if __name__ == "__main__":
