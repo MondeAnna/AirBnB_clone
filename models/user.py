576fd0f055fc5e822cdfba3932fdafc27df8ff62
MondeAnna <monde.anna@gmail.com>
2024-03-10 10:27:51 +0200

    user (attr): initialisation sets public attribute to empty strings

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+User Module: The definition, documentation and encapsulation of all common
+attributes and methods for the User class
+"""
+
+
+class User:
+
+    """
+    The definition, documentation and encapsulation of all common
+    attributes and methods for the User class
+    """
+
+    first_name = last_name = email = password = ""
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+"""
+Test suite for the user module
+"""
+
+
+from unittest.mock import MagicMock
+from unittest.mock import patch
+from unittest import TestCase
+from unittest import main
+
+
+from models import User
+
+
+class TestUser(TestCase):
+
+    """Collective testing of base model attributes"""
+
+    def setUp(self):
+        """Provide a factory for test instances"""
+
+        self.user = User()
+
+    def test_initialisation_has_empty_attr(self):
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+
+
+if __name__ == "__main__":
+    main()
